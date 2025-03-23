a91cd2bd65886f5af1e21ff41c1904c04595f3e9
nguyennguyen0 <trungnguyenwork123@gmail.com>
2025-03-23 20:35:25 +0700

    Add ui, controller, bus, component and other features from HuongBien to HuongBien2

---
+++ src/main/java/huongbien/dao/TableDAO.java
@@ -0,0 +1,181 @@
+package huongbien.dao;
+
+import huongbien.entity.Reservation;
+import huongbien.entity.Table;
+import huongbien.entity.TableStatus;
+import huongbien.jpa.PersistenceUnit;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@NoArgsConstructor
+public class RestaurantTableDAO extends GenericDAO<Table> {
+    public RestaurantTableDAO(PersistenceUnit persistenceUnit) {
+        super(persistenceUnit);
+    }
+
+    public List<Table> getAll() {
+        return findMany("SELECT t FROM RestaurantTable t", Table.class);
+    }
+
+    public Table getById(String id) {
+        return findOne("SELECT t FROM RestaurantTable t WHERE t.id = ?1", Table.class, id);
+    }
+
+    public List<Table> getByName(String name) {
+        return findMany("SELECT t FROM RestaurantTable t WHERE t.name LIKE ?1", Table.class, name + "%");
+    }
+
+    public List<Table> getAllByReservationId(String reservationId) {
+        return findMany("SELECT t FROM RestaurantTable t WHERE t.id IN (SELECT rt.table.id FROM ReservationTable rt WHERE rt.reservation.id = ?1)", Table.class, reservationId);
+    }
+
+    public List<Table> getAllByOrderId(String orderId) {
+        return findMany("SELECT t FROM RestaurantTable t WHERE t.id IN (SELECT ot.table.id FROM OrderTable ot WHERE ot.order.id = ?1)", Table.class, orderId);
+    }
+
+    public int getCountStatisticalOverviewTable() {
+        return count("SELECT COUNT(t) FROM RestaurantTable t WHERE t.status != 'Bàn đóng'");
+    }
+
+    public int getCountStatisticalOverviewTableEmpty() {
+        return count("SELECT COUNT(t) FROM RestaurantTable t WHERE t.status = 'Bàn trống'");
+    }
+
+    public int getCountStatisticalFloorTable(int floor) {
+        return count("SELECT COUNT(t) FROM RestaurantTable t WHERE t.status != 'Bàn đóng' AND t.floor = ?1", floor);
+    }
+
+    public int getCountStatisticalFloorTableEmpty(int floor) {
+        return count("SELECT COUNT(t) FROM RestaurantTable t WHERE t.status = 'Bàn trống' AND t.floor = ?1", floor);
+    }
+
+    public int getCountStatisticalFloorTablePreOrder(int floor) {
+        return count("SELECT COUNT(t) FROM RestaurantTable t WHERE t.status = 'Đặt trước' AND t.floor = ?1", floor);
+    }
+
+    public int getCountStatisticalFloorTableOpen(int floor) {
+        return count("SELECT COUNT(t) FROM RestaurantTable t WHERE t.status = 'Phục vụ' AND t.floor = ?1", floor);
+    }
+
+    public List<Table> getByCriteria(String floor, String status, String typeID, String seat) {
+        StringBuilder jpqlBuilder = new StringBuilder("SELECT t FROM RestaurantTable t WHERE t.status != 'Bàn đóng'");
+
+        if (!floor.isEmpty()) {
+            jpqlBuilder.append(" AND t.floor = ?1");
+        }
+
+        if (!status.isEmpty()) {
+            jpqlBuilder.append(" AND t.status = ?2");
+        }
+
+        if (!typeID.isEmpty()) {
+            jpqlBuilder.append(" AND t.tableType.id = ?3");
+        }
+
+        if (!seat.isEmpty()) {
+            jpqlBuilder.append(" AND t.seats = ?4");
+        }
+
+        return findMany(jpqlBuilder.toString(), Table.class, floor, status, typeID, seat);
+    }
+
+    public List<String> getDistinctFloor() {
+        return executeQuery("SELECT DISTINCT t.floor FROM RestaurantTable t", String.class);
+    }
+
+    public List<String> getDistinctSeat() {
+        return executeQuery("SELECT DISTINCT t.seats FROM RestaurantTable t", String.class);
+    }
+
+    public Table getTopFloor() {
+        return findOne("SELECT t FROM RestaurantTable t ORDER BY t.floor ASC", Table.class);
+    }
+
+    public List<Table> getReservedTables(LocalDate receiveDate) {
+        return findMany("SELECT t FROM RestaurantTable t WHERE t.reservations.receiveDate = ?1", Table.class, receiveDate);
+    }
+
+    public List<String> getDistinctStatuses() {
+        return executeQuery("SELECT DISTINCT t.status FROM RestaurantTable t WHERE t.status != 'Bàn đóng'", String.class);
+    }
+
+    public List<Integer> getDistinctFloors() {
+        return executeQuery("SELECT DISTINCT t.floor FROM RestaurantTable t", Integer.class);
+    }
+
+    public List<Integer> getDistinctSeats() {
+        return executeQuery("SELECT DISTINCT t.seats FROM RestaurantTable t", Integer.class);
+    }
+
+    public List<String> getDistinctTableType() {
+        return executeQuery("SELECT DISTINCT t.tableType.id FROM RestaurantTable t", String.class);
+    }
+
+    public List<Table> getLookUpTable(int floor, String name, int seat, String type, String status, int pageIndex) {
+        StringBuilder jpqlBuilder = new StringBuilder("SELECT t FROM RestaurantTable t WHERE t.name LIKE ?1 AND t.tableType.id LIKE ?2 AND t.status LIKE ?3");
+
+        if (floor != -1) {
+            jpqlBuilder.append(" AND t.floor = ?4");
+        }
+        if (seat != -1) {
+            jpqlBuilder.append(" AND t.seats = ?5");
+        }
+
+        return findMany(jpqlBuilder.toString(), Table.class, "%" + name + "%", "%" + type + "%", "%" + status + "%", floor, seat);
+    }
+
+    public int getCountLookUpTable(int floor, String name, int seat, String type, String status) {
+        StringBuilder jpqlBuilder = new StringBuilder("SELECT COUNT(t) FROM RestaurantTable t WHERE t.name LIKE ?1 AND t.tableType.id LIKE ?2 AND t.status LIKE ?3");
+
+        if (floor != -1) {
+            jpqlBuilder.append(" AND t.floor = ?4");
+        }
+        if (seat != -1) {
+            jpqlBuilder.append(" AND t.seats = ?5");
+        }
+
+        return count(jpqlBuilder.toString(), "%" + name + "%", "%" + type + "%", "%" + status + "%", floor, seat);
+    }
+
+    public List<Table> getAllWithPagination(int offset, int limit) {
+        String jpql = "SELECT t FROM Table t ORDER BY t.floor";
+        return findManyWithPagination(jpql, Table.class, null, offset, limit);
+    }
+
+    public List<Table> getByNameWithPagination(String name, int offset, int limit) {
+        String jpql = "SELECT t FROM Table t WHERE t.name LIKE :name ORDER BY t.floor";
+        return findManyWithPagination(jpql, Table.class, Map.of("name", "%" + name + "%"), offset, limit);
+    }
+
+    public List<Table> getByFloorWithPagination(int offset, int limit, int floor) {
+        String jpql = "SELECT t FROM Table t WHERE t.floor = :floor ORDER BY t.floor";
+        return findManyWithPagination(jpql, Table.class, Map.of("floor", floor), offset, limit);
+    }
+
+
+    public int countTotalTables() {
+        return count("SELECT COUNT(t) FROM RestaurantTable t");
+    }
+
+    public int countTotalTablesByFloor(int floor) {
+        return count("SELECT COUNT(t) FROM RestaurantTable t WHERE t.floor = ?1", floor);
+    }
+
+    public int countTotalTablesByName(String name) {
+        return count("SELECT COUNT(t) FROM RestaurantTable t WHERE t.name LIKE ?1", "%" + name + "%");
+    }
+
+    public List<Table> getListTableStatusToday(List<Reservation> reservationList) {
+//        TODO: Implement this method
+        return null;
+    }
+
+    public void updateStatusTable(String id, TableStatus status) {
+        Table table = getById(id);
+        table.setStatus(status);
+        update(table);
+    }
+}
